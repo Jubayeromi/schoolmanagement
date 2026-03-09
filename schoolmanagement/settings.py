fccafbfc0cd40bc86f2a00180ad33de96b28fa49
Jubayeromi <omijubayer10@gmail.com>
2026-03-09 15:35:18 +0600

    Update settings.py

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -14,6 +14,9 @@ import os
 import dj_database_url
 
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')

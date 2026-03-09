bd13347f9e0eeb1404064268931885e8321138fd
Jubayeromi <omijubayer10@gmail.com>
2026-03-09 23:49:13 +0600

    Update settings.py

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -36,7 +36,14 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '127.0.0.1')]
+# ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '127.0.0.1')]
+
+
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'schoolmanagement-1-5sfz.onrender.com'
+]
 
 
 

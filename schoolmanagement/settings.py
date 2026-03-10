891cfd3cce6adf3d72131332fbbbfd49208b4522
Jubayeromi <omijubayer10@gmail.com>
2026-03-10 12:23:05 +0600

    Update allowed host for deployment

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -42,7 +42,7 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    'schoolmanagement-1-5sfz.onrender.com'
+    'schoolmanagement-183t.onrender.com'
 ]
 
 
